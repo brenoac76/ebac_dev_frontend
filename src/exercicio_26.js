a255c0ad227c7833fe41bf79aaae2e5f644cb7f4
Breno Andrade Costa <breno.ac@gmail.com>
2024-02-21 00:05:58 -0300

    commit inicial

---
+++ src/exercicio_26.js
@@ -0,0 +1,7 @@
+"use strict";
+const numeros = (numero1, numero2) => numero1 * numero2;
+const numerosTotal = numeros(3, 5);
+console.log(numerosTotal);
+const saudacao = (nome) => `Olá ` + nome;
+const saudacao2 = saudacao('Breno');
+console.log(saudacao2);
---
+++ src/exercicio_26.ts
@@ -0,0 +1,15 @@
+const numeros = (numero1: number, numero2: number): number => numero1 * numero2
+
+const numerosTotal = numeros(3, 5);
+
+console.log(numerosTotal);
+
+const saudacao = (nome: string): string => `Olá ` + nome
+
+const saudacao2 = saudacao('Breno')
+
+console.log(saudacao2)
+
+
+
+
